29f544d5961f6a94697e984a062b48a528db93cb
shiiotaka <shiiotaka@gmail.com>
2021-03-29 23:07:08 +0900

    task_listにタイトルが表示されないテストの実装

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -99,7 +99,7 @@ RSpec.describe 'Task', type: :system do
         visit project_tasks_path(project)
         click_link 'Destroy'
         page.driver.browser.switch_to.alert.accept
-        expect(page).to have_content 'Task was successfully destroyed.'
+        expect(find('.task_list')).not_to have_content task.title
         expect(Task.count).to eq 0
         expect(current_path).to eq project_tasks_path(project)
       end
